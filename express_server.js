c0e9e55dd85e4ee224729a647d8f95f476c0dd08
amilford87 <amilford87@gmail.com>
2019-02-12 22:52:54 +0000

    added urls_new and have the form pushing to express_server.js and implemented a random letter generating function

--- express_server.js
+++ express_server.js
@@ -2,6 +2,14 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+    shortString = Math.random().toString(36).slice(2, 8);
+    return shortString;
+}
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -21,15 +29,26 @@ var urlDatabase = {
       let templateVars = { urls: urlDatabase };
       res.render("urls_index", templateVars);
   });
-
-  app.get("/urls/:shortURL", (req, res) => {
-      let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-      res.render("urls_show", templateVars);
-  });
+  
+  
+  app.get("/urls/new", (req, res) => {
+      res.render("urls_new");
+    });
+    
+    app.get("/urls/:shortURL", (req, res) => {
+        let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+        res.render("urls_show", templateVars);
+    });
+    
+    app.post("/urls", (req, res) => {
+        console.log(req.body);
+        res.send("Ok");
+    });
 
   app.get("/hello", (req, res) => {
       res.send("<html><body>Hello <b>World</b></body></html>");
   });
+
 
   app.listen(PORT, () => {
       console.log(`Example app listening on port ${PORT}!`);
