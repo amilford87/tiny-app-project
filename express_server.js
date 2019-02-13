35509102b88058eb638d427b67194258dbfef94a
amilford87 <amilford87@gmail.com>
2019-02-13 03:24:01 +0000

    added delete buttons that delete the corresponding url and return the user to the url/index

--- express_server.js
+++ express_server.js
@@ -52,6 +52,11 @@ var urlDatabase = {
         res.redirect("/urls/" + tinyUrl, 302);
         }
     });
+
+    app.post("/urls/:shortURL/delete/", (req, res) => {
+        delete urlDatabase[req.params.shortURL];
+        res.redirect("/urls/");
+    });
     
     app.get("/u/:shortURL", (req, res) => {
         const longURL = urlDatabase[req.params.shortURL];
